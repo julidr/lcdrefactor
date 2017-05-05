16ed38ea4fab3f4f7a4f1f86dfa49ba619699abf
Juliana Diaz <juli.milena@hotmail.com>
2017-05-04 19:57:22 -0500

    Adicion de Pruebas para ProcesadorIO

--- src/test/java/com/github/julidr/lcdrefactor/ProcesadorIOTest.java
+++ src/test/java/com/github/julidr/lcdrefactor/ProcesadorIOTest.java
@@ -42,5 +42,38 @@ public class ProcesadorIOTest {
 			assertEquals("Cadena " + input + " contiene mas caracteres ,", e.getMessage());
 		}
 	}
+	
+	@Test
+	public void debeLanzarExceptionPorQueElInputTieneMenosParametrosDeLosRequeridos(){
+		String input = "1,";
+		try {
+			procesadorIO.procesarInput(input);
+			fail("Debio lanzar un IllegalArgumentException por que el input tiene menos parametros de los requeridos");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Cadena " + input + " no contiene los parametros requeridos", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void debeLanzarExceptionPorQueElValorDelSizeNoEsUnNumero(){
+		String input = "a,1234";
+		try {
+			procesadorIO.procesarInput(input);
+			fail("Debio lanzar un IllegalArgumentException por que el valor del size no es un numero entero");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Parametro Size [a] no es un numero", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void debeLanzarExceptionPorQueElValorDelSizeSobrePasaElLimite(){
+		String input = "11,1234";
+		try {
+			procesadorIO.procesarInput(input);
+			fail("Debio lanzar un IllegalArgumentException por que el valor del size no esta entre 1 y 10");
+		} catch (IllegalArgumentException e) {
+			assertEquals("El parametro size [11] debe estar entre 1 y 10", e.getMessage());
+		}
+	}
 
 }
